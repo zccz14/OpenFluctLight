8ecaa39abe8306f86334e09b3972ff32c8eebdc8
CZ <cz@ntnl.io>
2026-03-05 12:12:05 +0800

    chore: prepare package for npm publish as openfluctlight

---
+++ index.js
@@ -0,0 +1,7 @@
+// OpenFluctLight - A lightweight fluctuation analysis library
+// This package is currently under development
+
+export default {
+  version: '0.0.1',
+  name: 'openfluctlight',
+};
